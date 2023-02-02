99a5a196555d322be2f4c9526c86892f25a7d39d
Filip Kalisz <fk276585@falmouth.ac.uk>
2023-02-02 11:43:33 +0000

    If implemented this commit will result in the barriers spawning around the player and withing the map
    The script creates a box around a player wthin the barriers can spawn, the barriers will spawn around all the players

--- Assets/Scripts/popUpBarriers/SummonBarrier.cs
+++ Assets/Scripts/popUpBarriers/SummonBarrier.cs
@@ -5,8 +5,9 @@ using UnityEngine.UIElements;
 
 public class SummonBarrier : MonoBehaviour
 {
-    //[Header("Floor")]
-    //[SerializeField] GameObject floor;
+    [Header("Floor")]
+    [SerializeField] GameObject floor;
+
 
     [Space]
     [Header("Barrier Prefabs")]
@@ -15,53 +16,63 @@ public class SummonBarrier : MonoBehaviour
     [SerializeField] GameObject barrierI;
     [SerializeField] GameObject barrierX;
 
+
     [Space]
     [Header("Height of barrier")]
     [SerializeField] float barrierHeight;
 
+
     [Space]
     [Header("Margin For Barrier Spawn Range")]
     [SerializeField] float margin;
+    [SerializeField] float marginPlayer;
+    [SerializeField] float rangePlayer;
 
-    [Space]
-    [Header("Map Range Values")]
-    [SerializeField] float xStart;
-    [SerializeField] float zStart;
-    [SerializeField] float xEnd;
-    [SerializeField] float zEnd;
 
     [Space]
     [Header("Range of time between barriers")]
     [SerializeField] int minTimeBetweenBarriers;
     [SerializeField] int maxTimeBetweenBarriers;
 
-    [Space]
-    [Header("Symmetry mode")]
-    public bool symetryMode;
 
-    float barrierxPopUpStart;
-    float barrierzPopUpStart;
-    float barrierxPopUpRange;
-    float barrierzPopUpRange;
+    float xStart;
+    float zStart;
+    float xEnd;
+    float zEnd;
+
+    Rect barrierSpawn;
+    Rect barrierLimit;
+    Rect barrierCoords;
+
 
 
     System.Random rand;
 
     GameObject[] barriers;
 
+    GameObject[] mechs;
+
     private void Start()
     {
+
+        Bounds floorSize = gameObject.GetComponent<MeshRenderer>().bounds;
         
+
+        xStart = floorSize.min.x;
+        xEnd = floorSize.max.x;
+        zStart = floorSize.min.z;
+        zEnd = floorSize.min.z;
+
+
         barriers = new GameObject[] { barrierL1, barrierL2, barrierI, barrierX };
 
-        barrierxPopUpStart = xStart + margin;
-        barrierzPopUpStart = zStart + margin;
-        barrierxPopUpRange = xEnd - margin;
-        barrierzPopUpRange = zEnd - margin;
+        mechs = GameObject.FindGameObjectsWithTag("Player");
+        
 
         StartCoroutine("SpawnBarrier");
 
     }
+
 
 
     IEnumerator SpawnBarrier()
@@ -70,23 +81,32 @@ public class SummonBarrier : MonoBehaviour
 
         int timeBetweenBarriers = rand.Next(minTimeBetweenBarriers, maxTimeBetweenBarriers);
 
-        int barrierIndex = rand.Next(0,barriers.Length);
+        //int barrierIndex = rand.Next(0, barriers.Length);
 
         int rotationOfBarrier = rand.Next(0, 3);
-        if (rotationOfBarrier == 0) { rotationOfBarrier = 0; }
-        else if (rotationOfBarrier == 1) { rotationOfBarrier = 90; }
-        else if (rotationOfBarrier == 2) { rotationOfBarrier = 180; }
-        else if (rotationOfBarrier == 3) { rotationOfBarrier = 270; }
+        rotationOfBarrier *= 90;
+
+
+        foreach (GameObject mech in mechs)
+        {
+
+            barrierCoords = new Rect(mech.transform.position.x, mech.transform.position.z, rangePlayer, rangePlayer);
+            Vector3 barrierSpawnRange = new Vector3(Random.Range(barrierCoords.yMin, barrierCoords.xMax), -barrierHeight, Random.Range(barrierCoords.yMin, barrierCoords.yMax));
+            barrierLimit = new Rect(floor.transform.position.x, floor.transform.position.z, floor.GetComponent<MeshRenderer>().bounds.extents.x - margin, floor.GetComponent<MeshRenderer>().bounds.extents.z - margin);
+            barrierSpawn = new Rect(mech.transform.position.x, mech.transform.position.z, marginPlayer, marginPlayer);
+
+            if (barrierLimit.Contains(barrierSpawnRange) && !barrierSpawn.Contains(barrierSpawnRange))
+            {
+                GameObject barrierToSpawn = Instantiate(barriers[rand.Next(0, barriers.Length)], barrierSpawnRange, Quaternion.identity);
+                barrierToSpawn.transform.Rotate(0, rotationOfBarrier, 0);
+            }
+            
+        }
 
         yield return new WaitForSeconds(timeBetweenBarriers);
 
-        if (!symetryMode)
-        {
-            GameObject barrierToSpawn = Instantiate(barriers[barrierIndex], new Vector3(Random.Range(barrierxPopUpStart, barrierxPopUpRange), -barrierHeight, Random.Range(barrierzPopUpStart, barrierzPopUpRange)), Quaternion.identity);
-            barrierToSpawn.transform.Rotate(0,rotationOfBarrier,0);
-                
-        }
-        
         StartCoroutine("SpawnBarrier");
     }
 }
+//GameObject barrierToSpawn = Instantiate(barriers[barrierIndex], Vector3.zero, Quaternion.identity);
+//barrierToSpawn.transform.Rotate(0, rotationOfBarrier, 0);
