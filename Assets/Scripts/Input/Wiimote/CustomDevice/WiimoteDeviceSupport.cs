604560c0ca54e987422b6e599fc970cb583a3d51
William Vickers Hastings <wvh200@gmail.com>
2023-02-03 15:16:41 +0000

    Added Comments to Wiimote Implementation

--- Assets/Scripts/Input/Wiimote/CustomDevice/WiimoteDeviceSupport.cs
+++ Assets/Scripts/Input/Wiimote/CustomDevice/WiimoteDeviceSupport.cs
@@ -9,21 +9,43 @@ using UnityEngine.InputSystem.Layouts;
  */
 namespace RedButton.Core.WiimoteSupport
 {
+    /// <summary>
+    /// Provides a way of alerting the Input System when a Wiimote is connect or disconnected from the computer.
+    /// </summary>
     [ExecuteInEditMode]
     public class WiimoteDeviceSupport : MonoBehaviour
     {
+        /// <summary>
+        /// Used to register the Wiimote Device Layerout with the Input System and set its interface for device description matching.
+        /// </summary>
+        private void Awake()
+        {
+            InputSystem.RegisterLayout<WiimoteDevice>(matches: new InputDeviceMatcher().WithInterface("WiimoteAPI"));
+        }
+
+        /// <summary>
+        /// Subscribe to the device added/removed events provided by the ControlArbiter.
+        /// </summary>
         private void OnEnable()
         {
             ControlArbiter.wiimoteAdded += OnDeviceAdded;
             ControlArbiter.wiimoteRemoved += OnDeviceRemoved;
         }
 
+        /// <summary>
+        /// Un-Subscribe to the device added/removed events provided by the ControlArbiter.
+        /// </summary>
         private void OnDisable()
         {
             ControlArbiter.wiimoteAdded -= OnDeviceAdded;
             ControlArbiter.wiimoteRemoved -= OnDeviceRemoved;
         }
 
+        /// <summary>
+        /// When a new wiimote is added this method is run, this adds the wiimote to the InputSystem
+        /// It does this with an InputDevice Description struct, that sets the name and interface type of the device.
+        /// </summary>
+        /// <param name="name">Wiimote device name (HID ID)</param>
         private void OnDeviceAdded(string name)
         {
             InputSystem.AddDevice(new InputDeviceDescription
@@ -33,6 +55,13 @@ namespace RedButton.Core.WiimoteSupport
             });
         }
 
+        /// <summary>
+        /// When a wiimote is removed this method is run, this makes sure to remove the right
+        /// wiimote from the InputSystem.
+        /// This is done by searching all devices for the first device that matches the InputDeviceDescription.
+        /// (the control arbiter keeps track of hte HID ID for this purposes.
+        /// </summary>
+        /// <param name="name">Target Wiimote HID ID</param>
         private void OnDeviceRemoved(string name)
         {
             var device = InputSystem.devices.FirstOrDefault(x => x.description == new InputDeviceDescription
@@ -47,9 +76,5 @@ namespace RedButton.Core.WiimoteSupport
             }
         }
 
-        private void Awake()
-        {
-            InputSystem.RegisterLayout<WiimoteDevice>(matches: new InputDeviceMatcher().WithInterface("WiimoteAPI"));
-        }
     }
 }
