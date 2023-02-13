776ff98ab7cfc478f220257d9aa6a7304ba24f3c
Sam Sheriff <ss279764@falmouth.ac.uk>
2023-02-13 15:37:23 +0000

    Added normal credits screen + started start UI

--- Assets/Scripts/UI Scripts/SamDoesScripts/StartMenuManagerScript.cs
+++ Assets/Scripts/UI Scripts/SamDoesScripts/StartMenuManagerScript.cs
@@ -7,24 +7,88 @@ public class StartMenuManagerScript : MonoBehaviour
 {
     [Header("Panels")]
     [SerializeField] GameObject bgPanel;
+    [SerializeField] GameObject bindPanel;
     [SerializeField] GameObject btnPanel;
     [SerializeField] GameObject creditsPanel;
+    [SerializeField] GameObject willCreditsPanel;
+    [SerializeField] GameObject playerNumPanel;
 
     [SerializeField] Sprite bgSprite;
     [SerializeField] Sprite willSprite;
 
+
+    private void Update()
+    {
+        if (bindPanel.activeSelf)
+        {
+            if (Input.anyKeyDown)
+            {
+                bindPanel.SetActive(false);
+                BindController();
+            }
+        }
+    }
+
+
+    public void BindController()
+    {
+        btnPanel.SetActive(true);
+        //controller binding goes here
+    }
+
+
+
+    public void OpenPlayerSelect()
+    {
+        btnPanel.SetActive(false);
+        playerNumPanel.SetActive(true);
+    }
+
+    public void ClosePlayerSelect()
+    {
+        btnPanel.SetActive(true);
+        playerNumPanel.SetActive(false);
+    }
+
+
+
+    public void OpenOptions()
+    {
+        
+    }
+    public void CloseOptions()
+    {
+
+    }
+
+
+
     public void OpenCredits()
     {
-        bgPanel.GetComponent<Image>().sprite = willSprite;
         btnPanel.SetActive(false);
         creditsPanel.SetActive(true);
     }
     public void CloseCredits()
     {
-        bgPanel.GetComponent<Image>().sprite = bgSprite;
         btnPanel.SetActive(true);
         creditsPanel.SetActive(false);
     }
+
+    //Will Credits
+    public void OpenWillCredits()
+    {
+        bgPanel.GetComponent<Image>().sprite = willSprite;
+        btnPanel.SetActive(false);
+        willCreditsPanel.SetActive(true);
+    }
+    public void CloseWillCredits()
+    {
+        bgPanel.GetComponent<Image>().sprite = bgSprite;
+        btnPanel.SetActive(true);
+        willCreditsPanel.SetActive(false);
+    }
+
+
 
     public void Quit()
     {
