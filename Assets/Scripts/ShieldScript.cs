fe92033cfee82f0ad295482459528e6f9fd62dbf
Filip Kalisz <fk276585@falmouth.ac.uk>
2023-02-15 13:28:27 +0000

    Progress into shields, can be activated

--- Assets/Scripts/ShieldScript.cs
+++ Assets/Scripts/ShieldScript.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShieldScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace RedButton.Mech
+{
+    public class ShieldScript : WeaponCore
     {
-        
+        [SerializeField] private GameObject shield;
+
+        protected override void BindtoControls()
+        {
+            ButtonEventContainer buttonEventContainer = controlBinding switch
+            {
+                ControlBinding.Fire1 => CMC.MechInputController.fireOneButton,
+                ControlBinding.Fire2 => CMC.MechInputController.fireTwoButton,
+                _ => null,
+            };
+
+            buttonEventContainer.OnButtonPressed += Fire;
+            buttonEventContainer.OnButtonReleased += UnFire;
+        }
+
+        protected override void UnBindControls()
+        {
+            ButtonEventContainer buttonEventContainer = controlBinding switch
+            {
+                ControlBinding.Fire1 => CMC.MechInputController.fireOneButton,
+                ControlBinding.Fire2 => CMC.MechInputController.fireTwoButton,
+                _ => null,
+            };
+
+            buttonEventContainer.OnButtonPressed -= Fire;
+            buttonEventContainer.OnButtonReleased -= UnFire;
+        }
+
+        private void UnFire()
+        {
+            Debug.Log("unfier");
+            shield.SetActive(false);
+        }
+
+        public override void Fire()
+        {
+            shield.SetActive(true);
+            Debug.Log("fier");
+        }
+
+        public override void GroupFire()
+        {
+            
+        }
+
+        protected override void Update()
+        {
+            
+        }
     }
 }
