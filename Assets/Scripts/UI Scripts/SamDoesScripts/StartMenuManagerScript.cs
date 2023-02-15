7205ba1b2462abbaaeb2db93b300c240ed620267
Sam Sheriff <ss279764@falmouth.ac.uk>
2023-02-15 13:23:21 +0000

    Main menu complete and working with controllers

--- Assets/Scripts/UI Scripts/SamDoesScripts/StartMenuManagerScript.cs
+++ Assets/Scripts/UI Scripts/SamDoesScripts/StartMenuManagerScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class StartMenuManagerScript : MonoBehaviour
 {
@@ -9,9 +10,20 @@ public class StartMenuManagerScript : MonoBehaviour
     [SerializeField] GameObject bgPanel;
     [SerializeField] GameObject bindPanel;
     [SerializeField] GameObject btnPanel;
+    [SerializeField] GameObject optionsPanel;
     [SerializeField] GameObject creditsPanel;
     [SerializeField] GameObject willCreditsPanel;
     [SerializeField] GameObject playerNumPanel;
+    [SerializeField] GameObject lvlSelectPanel;
+
+    [Header("Buttons")]
+    [SerializeField] GameObject btnPanelBtn;
+    [SerializeField] GameObject optionsPanelBtn;
+    [SerializeField] GameObject creditsPanelBtn;
+    [SerializeField] GameObject playerNumPanelBtn;
+    [SerializeField] GameObject lvlSelectPanelBtn;
+
+    [SerializeField] EventSystem eventSystem;
 
     [SerializeField] Sprite bgSprite;
     [SerializeField] Sprite willSprite;
@@ -33,6 +45,7 @@ public class StartMenuManagerScript : MonoBehaviour
     public void BindController()
     {
         btnPanel.SetActive(true);
+        eventSystem.SetSelectedGameObject(btnPanelBtn);
         //controller binding goes here
     }
 
@@ -42,23 +55,52 @@ public class StartMenuManagerScript : MonoBehaviour
     {
         btnPanel.SetActive(false);
         playerNumPanel.SetActive(true);
+        eventSystem.SetSelectedGameObject(playerNumPanelBtn);
     }
 
     public void ClosePlayerSelect()
     {
         btnPanel.SetActive(true);
         playerNumPanel.SetActive(false);
+        eventSystem.SetSelectedGameObject(btnPanelBtn);
+    }
+
+
+
+    public void OpenLvlSelect(int playerNum)
+    {
+        playerNumPanel.SetActive(false);
+        lvlSelectPanel.SetActive(true);
+        Debug.Log(playerNum);
+        eventSystem.SetSelectedGameObject(lvlSelectPanelBtn);
+    }
+
+    public void lvlBtnClick(int lvl)
+    {
+        Debug.Log("Selected level is "+lvl);
+    }
+
+    public void CloseLvlSelect()
+    {
+        playerNumPanel.SetActive(true);
+        lvlSelectPanel.SetActive(false);
+        bgPanel.GetComponent<Image>().sprite = bgSprite;
+        eventSystem.SetSelectedGameObject(playerNumPanelBtn);
     }
 
 
 
     public void OpenOptions()
     {
-        
+        btnPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+        eventSystem.SetSelectedGameObject(optionsPanelBtn);
     }
     public void CloseOptions()
     {
-
+        btnPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+        eventSystem.SetSelectedGameObject(btnPanelBtn);
     }
 
 
@@ -67,11 +109,13 @@ public class StartMenuManagerScript : MonoBehaviour
     {
         btnPanel.SetActive(false);
         creditsPanel.SetActive(true);
+        eventSystem.SetSelectedGameObject(creditsPanelBtn);
     }
     public void CloseCredits()
     {
         btnPanel.SetActive(true);
         creditsPanel.SetActive(false);
+        eventSystem.SetSelectedGameObject(btnPanelBtn);
     }
 
     //Will Credits
