ff75c5d4ebd56b084b49f68140c966ed1016b97d
William Vickers Hastings <WV276829@falmouth.ac.uk>
2023-03-09 11:38:38 +0000

    Added Power Ups Manager. Bug Power Ups can only be consumed once before they stop reappearing

---
+++ Assets/Scripts/PowerUps/MapPowerUp.cs
@@ -0,0 +1,118 @@
+using RedButton.Mech;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace RedButton.GamePlay
+{
+    public class MapPowerUp : MonoBehaviour
+    {
+        [SerializeField] private PowerUpCore powerUpCore;
+        [HideInInspector] public PowerUpsManager manager;
+        [SerializeField] private bool mechInTrigger;
+        [SerializeField] private bool powerUpActive;
+        [SerializeField] private float spinSpeed = 1;
+
+        private float SpinAngle => Time.deltaTime * UnityEngine.Random.value * spinSpeed;
+
+        float powerUpTimeOut;
+        float countUpTimer;
+
+        MeshRenderer capsuleRenderer;
+        Transform powerUpCapsule;
+
+        private void Awake()
+        {
+            capsuleRenderer = GetComponentInChildren<MeshRenderer>();
+            //capsuleRenderer.enabled = false;
+            powerUpCapsule = capsuleRenderer.transform;
+            powerUpCore = GetComponent<PowerUpCore>();
+            
+        }
+
+        private void Update()
+        {
+            powerUpCapsule.Rotate(SpinAngle, SpinAngle, SpinAngle, Space.Self);
+        }
+
+        
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(countUpTimer > 0)
+            {
+                return;
+            }
+            CentralMechComponent mech = other.gameObject.GetComponentInParent<CentralMechComponent>();
+
+            if(mech != null)
+            {
+                powerUpCore.AddTo(mech);
+                ForceRespawn();
+            }
+
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            mechInTrigger = true;
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (countUpTimer > 0)
+            {
+                countUpTimer = 0;
+                powerUpTimeOut = manager.GetRespawnTime();
+            }
+            mechInTrigger = false;
+        }
+
+        public void ForceRespawn()
+        {
+            capsuleRenderer.enabled = false;
+            if(powerUpCore != null)
+            {
+                manager.activePowerUps.Remove(this);
+                manager.inactivePowerUps.Add(this);
+                manager.ReleasePowerUp(powerUpCore);
+                Destroy(powerUpCore);
+            }
+            StartCoroutine(ReSpawner());
+        }
+
+        private IEnumerator ReSpawner()
+        {
+            powerUpTimeOut = manager.GetRespawnTime();
+            countUpTimer = 0;
+            while(countUpTimer < powerUpTimeOut)
+            {
+                while (mechInTrigger)
+                {
+                    yield return null;
+                }
+                countUpTimer += Time.deltaTime;
+                yield return null;
+            }
+
+            countUpTimer = 0;
+            PowerUpCore powerUpCore = manager.GetPowerUp();
+            if (powerUpCore != null)
+            {
+                this.powerUpCore = (PowerUpCore)gameObject.AddComponent(powerUpCore.GetType());
+                this.powerUpCore.Copy(powerUpCore);
+                manager.activePowerUps.Add(this);
+                manager.inactivePowerUps.Remove(this);
+                capsuleRenderer.material.color = powerUpCore.powerUpColour;
+                capsuleRenderer.enabled = true;
+
+            }
+            else
+            {
+                manager.activePowerUps.Remove(this);
+                manager.inactivePowerUps.Add(this);
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/PowerUps/PowerUpsManager.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RedButton.GamePlay
+{
+    public class PowerUpsManager : MonoBehaviour
+    {
+        [SerializeField] private Vector3[] spawnPoints = new Vector3[4];
+        [SerializeField] private PowerUpCore[] powerUps;
+        [SerializeField] private MapPowerUp powerUpPrefab;
+        [SerializeField] private List<MapPowerUp> powerUpInstances;
+        [SerializeField] private Dictionary<Type, int> onMapPowerUps = new();
+        [SerializeField] private int globalCountOffset;
+        private int maxPowerUps;
+        [SerializeField] private int powerUpActivateWaveSize;
+        [SerializeField] private float powerUpStartOfRoundDelay = 5f;
+        public readonly HashSet<MapPowerUp> activePowerUps = new();
+        public readonly HashSet<MapPowerUp> inactivePowerUps = new();
+        
+        public float minPowerUpTime;
+        public float maxPowerUpTime;
+        private void Awake()
+        {
+            maxPowerUps = spawnPoints.Length;
+        }
+
+        public void SetUpPowerUps()
+        {
+            if (powerUpInstances.Count!= 0)
+            {
+                StopAllCoroutines();
+                powerUpInstances.ForEach(powerUpInstance => Destroy(powerUpInstance.gameObject));
+                powerUpInstances.Clear();
+                onMapPowerUps.Clear();
+                inactivePowerUps.Clear();
+                activePowerUps.Clear();
+            }
+
+            for (int i = 0; i < spawnPoints.Length; i++)
+            {
+                powerUpInstances.Add(Instantiate(powerUpPrefab, spawnPoints[i], Quaternion.identity, transform));
+                powerUpInstances[^1].manager = this;
+            }
+
+            for (int i = 0; i < powerUps.Length; i++)
+            {
+                onMapPowerUps.Add(powerUps[i].GetType(), 0);
+            }
+            inactivePowerUps.UnionWith(powerUpInstances);
+
+            StartCoroutine(Initializer());
+        }
+
+        private IEnumerator Initializer()
+        {
+            yield return new WaitForSeconds(powerUpStartOfRoundDelay);
+            List<MapPowerUp> inactive = new(inactivePowerUps);
+            while (activePowerUps.Count < maxPowerUps) {
+                for (int i = 0; i < powerUpActivateWaveSize; i++)
+                {
+                    if (inactive.Count == 0)
+                    {
+                        break;
+                    }
+                    MapPowerUp powerUp = inactive[UnityEngine.Random.Range(0, inactive.Count)];
+                    while (activePowerUps.Contains(powerUp))
+                    {
+                        powerUp = inactive[UnityEngine.Random.Range(0, inactive.Count)];
+                    }
+                    inactive.Remove(powerUp);
+                    powerUp.ForceRespawn();
+                }
+                yield return null;
+            }
+        }
+
+        public float GetRespawnTime()
+        {
+            return UnityEngine.Random.Range(minPowerUpTime, maxPowerUpTime);
+        }
+
+        public PowerUpCore GetPowerUp()
+        {
+            List<Type> types = new(onMapPowerUps.Keys);
+            PowerUpCore lowestType = null;
+            int count = int.MaxValue;
+            Type type = null;
+            for (int i = 0; i < types.Count; i++)
+            {
+                if (onMapPowerUps[types[i]] <= count && onMapPowerUps[types[i]] < powerUps[i].limit + globalCountOffset)
+                {
+                    type = types[i];
+                    lowestType = powerUps[i];
+                    count = onMapPowerUps[types[i]];
+                }
+            }
+            if(lowestType != null)
+            {
+                onMapPowerUps[type]++;
+            }
+            return lowestType;
+        }
+
+        public void ReleasePowerUp(PowerUpCore powerUp)
+        {
+            onMapPowerUps[powerUp.GetType()] = onMapPowerUps[powerUp.GetType()] <= 0 ? 0 : onMapPowerUps[powerUp.GetType()]--;
+        }
+    }
+}
