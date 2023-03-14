a82274d8fa30a79164ed2815ff245bb2bc43cce3
Filip Kalisz <FK276585@falmouth.ac.uk>
2023-03-14 18:28:41 +0000

    Old barrier system

--- Assets/Scripts/popUpBarriers/SummonBarrier.cs
+++ Assets/Scripts/popUpBarriers/SummonBarrier.cs
@@ -94,69 +94,69 @@ public class SummonBarrier : MonoBehaviour
         
         int rotationOfBarrier = Random.Range(0, 3);
         rotationOfBarrier *= 90;
-       
-        for (int i = 1; i < numberOfBarriers; i++)
+
+        //for (int i = 1; i < numberOfBarriers; i++)
+        //{
+        //    //List<Vector3> avoidVectors = new List<Vector3>();
+        //    Vector3 spawnBarrierRange = new Vector3(Random.Range(floorSize.min.x + margin, floorSize.max.x - margin), floor.transform.position.y, Random.Range(floorSize.min.z + margin, floorSize.max.z - margin));
+        //    GameObject barrierToSpawn = Instantiate(barriers[Random.Range(0, barriers.Length)], spawnBarrierRange - new Vector3(0, barrierHeight, 0), Quaternion.identity);
+        //    barrierToSpawn.transform.Rotate(0, rotationOfBarrier, 0);
+        //    //if (avoidVectors != null)
+        //    //{
+        //    //    foreach (Vector3 avoidVector in avoidVectors)
+        //    //    {
+        //    //        Vector3 newRect = avoidVector - new Vector3(rangePlayer, 0, rangePlayer);
+        //    //        barrierLimit = new Rect(newRect.x, newRect.z, rangePlayer * 2, rangePlayer * 2);
+        //    //        if (barrierLimit.Contains(spawnBarrierRange))
+        //    //        {
+        //    //            break;
+        //    //        }
+        //    //        else
+        //    //        {
+        //    //            GameObject barrierToSpawn = Instantiate(barriers[Random.Range(0, barriers.Length)], spawnBarrierRange - new Vector3(0, barrierHeight, 0), Quaternion.identity);
+        //    //            barrierToSpawn.transform.Rotate(0, rotationOfBarrier, 0);
+        //    //        }
+        //    //    }
+        //    //}
+        //    //else
+        //    //{
+        //    //    GameObject barrierToSpawn = Instantiate(barriers[Random.Range(0, barriers.Length)], spawnBarrierRange - new Vector3(0, barrierHeight, 0), Quaternion.identity);
+        //    //    barrierToSpawn.transform.Rotate(0, rotationOfBarrier, 0);
+        //    //}
+        //    //avoidVectors.Add(spawnBarrierRange);
+        //}
+
+        foreach (GameObject mech in mechs)
         {
-            //List<Vector3> avoidVectors = new List<Vector3>();
-            Vector3 spawnBarrierRange = new Vector3(Random.Range(floorSize.min.x + margin, floorSize.max.x - margin), floor.transform.position.y, Random.Range(floorSize.min.z + margin, floorSize.max.z - margin));
-            GameObject barrierToSpawn = Instantiate(barriers[Random.Range(0, barriers.Length)], spawnBarrierRange - new Vector3(0, barrierHeight, 0), Quaternion.identity);
-            barrierToSpawn.transform.Rotate(0, rotationOfBarrier, 0);
-            //if (avoidVectors != null)
-            //{
-            //    foreach (Vector3 avoidVector in avoidVectors)
-            //    {
-            //        Vector3 newRect = avoidVector - new Vector3(rangePlayer, 0, rangePlayer);
-            //        barrierLimit = new Rect(newRect.x, newRect.z, rangePlayer * 2, rangePlayer * 2);
-            //        if (barrierLimit.Contains(spawnBarrierRange))
-            //        {
-            //            break;
-            //        }
-            //        else
-            //        {
-            //            GameObject barrierToSpawn = Instantiate(barriers[Random.Range(0, barriers.Length)], spawnBarrierRange - new Vector3(0, barrierHeight, 0), Quaternion.identity);
-            //            barrierToSpawn.transform.Rotate(0, rotationOfBarrier, 0);
-            //        }
-            //    }
-            //}
-            //else
-            //{
-            //    GameObject barrierToSpawn = Instantiate(barriers[Random.Range(0, barriers.Length)], spawnBarrierRange - new Vector3(0, barrierHeight, 0), Quaternion.identity);
-            //    barrierToSpawn.transform.Rotate(0, rotationOfBarrier, 0);
-            //}
-            //avoidVectors.Add(spawnBarrierRange);
+
+            barrierCoords = new Rect(mech.transform.position.x, mech.transform.position.z, rangePlayer, rangePlayer);
+            Debug.Log(barrierCoords);
+            Vector3 barrierSpawnRange = new Vector3(Random.Range(barrierCoords.xMin, barrierCoords.xMax), floor.transform.position.y, Random.Range(barrierCoords.yMin, barrierCoords.yMax));
+            Debug.Log(barrierSpawnRange);
+            barrierLimit = new Rect(floor.transform.position.x, floor.transform.position.z, floor.GetComponent<MeshCollider>().bounds.extents.x - margin, floor.GetComponent<MeshCollider>().bounds.extents.z - margin);
+            Debug.Log(barrierLimit);
+
+
+            if (floorSize.Contains(barrierSpawnRange))
+            {
+                Debug.Log("Got To Point A");
+                if (!guaranteeBarriers)
+                {
+                    int likelyhoodOfBarrier = Random.Range(0, upperBound);
+                    if (likelyhoodOfBarrier != 0)
+                    {
+                        GameObject barrierToSpawn = Instantiate(barriers[Random.Range(0, barriers.Length)], barrierSpawnRange - new Vector3(0, barrierHeight, 0), Quaternion.identity);
+                        barrierToSpawn.transform.Rotate(0, rotationOfBarrier, 0);
+                    }
+                }
+                else
+                {
+                    GameObject barrierToSpawn = Instantiate(barriers[Random.Range(0, barriers.Length)], barrierSpawnRange - new Vector3(0, barrierHeight, 0), Quaternion.identity);
+                    barrierToSpawn.transform.Rotate(0, rotationOfBarrier, 0);
+                }
+            }
+
         }
-
-        //    foreach (GameObject mech in mechs)
-        //    {
-
-        //        barrierCoords = new Rect(mech.transform.position.x, mech.transform.position.z, rangePlayer, rangePlayer);
-        //        Debug.Log(barrierCoords);
-        //        Vector3 barrierSpawnRange = new Vector3(Random.Range(barrierCoords.xMin, barrierCoords.xMax), floor.transform.position.y, Random.Range(barrierCoords.yMin, barrierCoords.yMax));
-        //        Debug.Log(barrierSpawnRange);
-        //        barrierLimit = new Rect(floor.transform.position.x, floor.transform.position.z, floor.GetComponent<MeshCollider>().bounds.extents.x - margin, floor.GetComponent<MeshCollider>().bounds.extents.z - margin);
-        //        Debug.Log(barrierLimit);
-
-
-        //        if (floorSize.Contains(barrierSpawnRange))
-        //        {
-        //            Debug.Log("Got To Point A");
-        //            if (!guaranteeBarriers)
-        //            {
-        //                int likelyhoodOfBarrier = Random.Range(0, upperBound);
-        //                if (likelyhoodOfBarrier != 0)
-        //                {
-        //                    GameObject barrierToSpawn = Instantiate(barriers[Random.Range(0, barriers.Length)], barrierSpawnRange - new Vector3(0, barrierHeight, 0), Quaternion.identity);
-        //                    barrierToSpawn.transform.Rotate(0, rotationOfBarrier, 0);
-        //                }
-        //            }
-        //            else
-        //            {
-        //                GameObject barrierToSpawn = Instantiate(barriers[Random.Range(0, barriers.Length)], barrierSpawnRange - new Vector3(0, barrierHeight, 0), Quaternion.identity);
-        //                barrierToSpawn.transform.Rotate(0, rotationOfBarrier, 0);
-        //            }
-        //        }
-
-        //    }
 
         yield return new WaitForSeconds(timeBetweenBarriers);
 
