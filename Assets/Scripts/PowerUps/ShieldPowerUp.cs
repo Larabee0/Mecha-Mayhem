d090ff4070e75bc9553726172cb9afdaa3edbf6c
William Vickers Hastings <wvh200@gmail.com>
2023-03-15 17:13:34 +0000

    Added Weapon Power Up

--- Assets/Scripts/PowerUps/ShieldPowerUp.cs
+++ Assets/Scripts/PowerUps/ShieldPowerUp.cs
@@ -7,14 +7,32 @@ namespace RedButton.GamePlay
     public class ShieldPowerUp : PowerUpCore
     {
         [SerializeField] private int shieldExtraHealth = 3;
+        [SerializeField] private bool RechargeShieldNowIfDischarged = false;
         public override void Copy(PowerUpCore source)
         {
-            base.Copy(source);
             shieldExtraHealth = (source as ShieldPowerUp).shieldExtraHealth;
         }
+        
         public override void AddTo(CentralMechComponent target)
         {
-            target.shield.currentShieldHealth = shieldExtraHealth;
+            int curHealth = target.shield.currentShieldHealth;
+
+            switch (curHealth)
+            {
+                case 0:
+                    curHealth = shieldExtraHealth;
+                    break;
+                case > 0 when (curHealth <= target.shield.MaxShieldHealth && target.ShieldActive)||(curHealth <= shieldExtraHealth && !target.ShieldActive):
+                    curHealth += shieldExtraHealth;
+                    break;
+            }
+
+            target.shield.currentShieldHealth = curHealth;
+            target.shield.ShieldColour();
+            if(RechargeShieldNowIfDischarged)
+            {
+                target.shield.RechargeNow();
+            }
         }
     }
 }
