7d279cfdf718d025ac693ab281592ab2529d9eb4
Filip Kalisz <fk276585@falmouth.ac.uk>
2023-04-27 11:07:58 +0100

    Removed Debug.Log

--- Assets/Scripts/popUpBarriers/BarriersGoDown.cs
+++ Assets/Scripts/popUpBarriers/BarriersGoDown.cs
@@ -36,8 +36,6 @@ public class BarriersGoDown : MonoBehaviour
 
         barriers = new List<GameObject>();
         barriers.AddRange(GameObject.FindGameObjectsWithTag("Barrier"));
-        Debug.Log(barriers[2]);
-
 
         StartCoroutine(BarriersMovingDown());
         
